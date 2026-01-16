17ef137172798fe902fa950dec50553832052dd4
Moti-Sultan <motisultannur@gmail.com>
2026-01-16 15:51:36 +0100

    refactor(auth): set valid redirect to dashboard after login and registration

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+
+final class DashboardController extends AbstractController
+{
+    #[Route('/dashboard_freelancer', name: 'dashboard_freelancer')]
+    
+    #[IsGranted('ROLE_USER')]
+    
+    public function index(): Response
+    {
+       
+        return $this->render('dashboard/index.html.twig', [
+            'controller_name' => 'DashboardController',
+        ]);
+    }
+}
