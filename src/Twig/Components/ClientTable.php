8473c9ff85e543a7fd76668c050ec95b3b1c5863
sultan8080 <118168309+sultan8080@users.noreply.github.com>
2026-01-21 14:05:49 +0100

    feat(client): Modify ClientTable Classe to connect search input to database

--- src/Twig/Components/ClientTable.php
+++ src/Twig/Components/ClientTable.php
@@ -2,11 +2,31 @@
 
 namespace App\Twig\Components;
 
+use App\Repository\ClientRepository;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
+use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
 
 #[AsLiveComponent]
-final class ClientTable
+class ClientTable
 {
     use DefaultActionTrait;
+
+    #[LiveProp(writable: true)] // This allows the search input to change this value
+    public string $query = '';
+
+    public function __construct(
+        private ClientRepository $clientRepository,
+        private Security $security
+    ) {}
+
+    public function getClients(): array
+    {
+        // This is called automatically every time 'query' changes
+        return $this->clientRepository->findBySearch(
+            $this->security->getUser(),
+            $this->query
+        );
+    }
 }
