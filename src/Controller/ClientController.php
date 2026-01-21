591399d36540f896839930468a8de6dd35c8228c
sultan8080 <118168309+sultan8080@users.noreply.github.com>
2026-01-21 22:41:31 +0100

    feat: new client form updated

--- src/Controller/ClientController.php
+++ src/Controller/ClientController.php
@@ -38,7 +38,7 @@ final class ClientController extends AbstractController
         ]);
     }
 */
-    #[Route('/new', name: 'app_client_new', methods: ['GET', 'POST'])]
+    #[Route('/new_client', name: 'app_client_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $client = new Client();
