0ef25479fecca78a5d9aa2d3d8d76fc786382f1b
sultan8080 <118168309+sultan8080@users.noreply.github.com>
2026-01-23 16:44:30 +0100

    fixtures: Upate data fixtures as per invoice update

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -21,7 +21,6 @@ class AppFixtures extends Fixture
 
     public function load(ObjectManager $manager): void
     {
-        // 1. Create 5 Users
         for ($i = 1; $i <= 2; $i++) {
             $user = new User();
             $user->setEmail("user$i@test.com");
@@ -35,7 +34,6 @@ class AppFixtures extends Fixture
 
             $manager->persist($user);
 
-            // 2. Create 5 Clients for EACH User
             for ($j = 1; $j <= 2; $j++) {
                 $client = new Client();
                 $client->setFirstName("prenom$j");
@@ -43,28 +41,32 @@ class AppFixtures extends Fixture
                 $client->setCompanyName("Client Company $i-$j");
                 $client->setPhoneNumber("+3312345678$j");
                 $client->setAddress("$j Rue de Exemple, 7500$j Paris, France");
-
                 $client->setEmail("client$j.from.user$i@example.com");
                 $client->setUser($user);
+                
                 $manager->persist($client);
 
-                // 3. Create 5 Invoices for EACH Client
                 for ($k = 1; $k <= 5; $k++) {
                     $invoice = new Invoice();
-                    $invoice->setInvoiceNumber("FAC-2026-" . str_pad((string)($i * $j * $k), 3, '0', STR_PAD_LEFT));
+                    // Link to BOTH client and user
+                    $invoice->setClient($client);
+                    $invoice->setUser($user); // <--- NEW DIRECT LINK
+                    
+                    $invoice->setProjectTitle("Project $k for Client $j"); // <--- NEW FIELD
+                    $invoice->setInvoiceNumber("FAC-2026-" . str_pad((string)(($i-1)*10 + ($j-1)*5 + $k), 3, '0', STR_PAD_LEFT));
                     $invoice->setTotalAmount(strval(rand(100, 5000)));
                     $invoice->setCurrency('EUR');
                     $invoice->setStatus('sent');
-                    $invoice->setClient($client);
+                    
                     $manager->persist($invoice);
 
-                    // 4. Create 5 Items for EACH Invoice
                     for ($l = 1; $l <= 5; $l++) {
                         $item = new InvoiceItem();
                         $item->setDescription("Service delivery line $l");
                         $item->setQuantity((float)rand(1, 10));
                         $item->setUnitPrice(strval(rand(50, 500)));
                         $item->setInvoice($invoice);
+                        
                         $manager->persist($item);
                     }
                 }
