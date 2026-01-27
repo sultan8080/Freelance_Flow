1a81a9406eb387ad39bb76a34af2b214e3502e94
sultan8080 <118168309+sultan8080@users.noreply.github.com>
2026-01-27 18:34:01 +0100

    build(assets): register new form-collection controller

--- assets/stimulus_bootstrap.js
+++ assets/stimulus_bootstrap.js
@@ -1,7 +1,8 @@
 import { startStimulusApp } from '@symfony/stimulus-bundle';
 import FlashController from './controllers/flash_controller.js';
-
+import FormCollectionController from './controllers/form-collection_controller.js';
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
 app.register('flash', FlashController);
+app.register('form-collection', FormCollectionController);
